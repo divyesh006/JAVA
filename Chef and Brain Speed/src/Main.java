a67585f231a8f4a5bbf0ce423447339fc59373dc
divyesh006 <divyesh10008@gmail.com>
2022-07-20 12:14:34 +0530

    Codechef

---
+++ Chef and Brain Speed/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        if(y>x){
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+    }
+}
