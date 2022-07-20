3636e3064302cf053e17a455e111751373c63360
divyesh006 <divyesh10008@gmail.com>
2022-07-20 18:29:22 +0530

    Codechef

---
+++ The Cheaper Cab/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            if (x == y){
+                System.out.println("Any");
+            } else if (x > y) {
+                System.out.println("Second");
+            }else {
+                System.out.println("First");
+            }
+        }
+    }
+}
