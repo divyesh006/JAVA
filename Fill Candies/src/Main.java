1dc7666f86fa6e071c5912421e10e4480a5c50a6
divyesh006 <divyesh10008@gmail.com>
2022-07-21 11:18:24 +0530

    Codechef

---
+++ Fill Candies/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int m = sc.nextInt();
+            fo
+
+        }
+    }
+}
---
+++ Passes for Fair/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            if(k>n){
+                System.out.println("Yes");
+            }
+            else {
+                System.out.println("No");
+            }
+        }
+    }
+}
